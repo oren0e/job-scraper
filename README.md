69ab70144370177027874d6a63aad78c40f02345
Oren Epshtain <oren@gojuno.com>
2020-04-15 19:16:17 +0300

    changed readme file, added if name main line to command script, edited requirements.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Overview
-A command-line interface to scrape jobs from Indeed.com with terms search and emailing options.
+A command-line interface to scrape jobs from Indeed.com with search terms. It is configured to look for jobs in Israel.  
+**Note:** the tags in Indeed website can change and if it does you will see that no results are being found and the Dataframe
+that comes back is `None`.
+
+Currently this version works with Unix-based systems only (tested on MacOS with python 3.7) 
+
+# Installation
+1. This package uses `Selenium` which can emulate browser usage within python. You need to install a correct driver for 
+your browser in addition to the `selenium` package. Please refer to https://pypi.org/project/selenium/ for further details.
+2. Clone the repo to your local machine.
+3. Give execution permissions to `jobs.py` like so: `$ chmod +x jobs.py`
+
+# Usage
+You can always issue `$ ./jobs.py -h` for help and usage instructions.  
+
+For example, in order to grab the 30 first (sorted by date with newest first) jobs for "data scientist" query
+and keep only the jobs that have at least one of "machine learning, engineer, python" keywords (separated by `,`) inside
+the (description + requirements) text you would type:  
+`$ ./jobs.py "data scientist" -t "machine learning, engineer, python" -n 30`  
+If you wanted the first 30 jobs to be sorted by relevance instead, you would add `-sbr` to the former.  
+And if you wanted to keep only the jobs that have **all** the keywords in the text you would add `-a` to the command above.
+
+## Output and Logs
+### Output
+The output is an HTML table with job title, company, text, and a clickable link as its columns. This table is being saved as
+`jobs_[Current_Date].html` to your main folder where you cloned the repo to.
+Current_Date is the current date like so: 2020-03-14.
+
+### Logs
+The command keeps logs for different stages of the process. There is one log file for each date so if you run this couple of
+times at the same day, the logs get appended to the same file. The logs are saved in the `logs` folder under you main folder
+where you cloned the repo to.
+
+# TODO
+1. Specify the country in which to look. This could possibly have issues with html tags compatibility across different indeed
+country-specific sites.
+2. Add option to send jobs file to email. Potential problem (for the free options) when using Gmail is a trade-off
+between allowing non-secure connections to your Gmail or generating a refresh token that will expire once in a while,
+so the solution is not robust enough.
+3. Optional - install the `jobs.py` script to system PATH.
+4. Add example for usage with cron
+5. Make the code work with Windows
+  
--- jobs.py
+++ jobs.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python
-from core_files.backend import Notifier
-import argparse
-from config.common import ROOT_DIR, CURRENT_DATE
-from utils.logger import logger
 
-# CLI
-parser = argparse.ArgumentParser(prog='jobs',
-                                 description='Get html table of recent jobs from indeed.com'
-                                             ' according to query and optional'
-                                             ' search terms in jobs description')
-# arguments
-parser.add_argument('query', metavar='query', type=str, help='Query string. Example: "software engineer"')
-parser.add_argument('-t', '--terms', type=str, action='store', help='Search terms for job description separated by ",".'
-                                                                    'Example: "dog, cat lover, high quality"')
-parser.add_argument('-n', '--num_jobs', action='store', type=int, help='Number of search results to fetch. Default: 15')
-parser.add_argument('-sbr', '--sortby_relevance', action='store_true',
-                    help='Sort by relevance. Default is by date')
-parser.add_argument('-a', '--all_terms', action='store_true',
-                    help='If specified, all search terms must be found in job description')
-parser.set_defaults(sortby_relevance=False)
-args = parser.parse_args()
+if __name__ == '__main__':
+    from core_files.backend import Notifier
+    import argparse
+    from config.common import ROOT_DIR, CURRENT_DATE
+    from utils.logger import logger
 
-if args.sortby_relevance:
-    myjob = Notifier(query=args.query, terms=args.terms, num_recent_jobs=args.num_jobs,
-                     sort_by='relevance', all_terms=args.all_terms)
-else:
-    myjob = Notifier(query=args.query, terms=args.terms, num_recent_jobs=args.num_jobs,
-                     all_terms=args.all_terms)
+    # CLI
+    parser = argparse.ArgumentParser(prog='jobs',
+                                     description='Get html table of recent jobs from indeed.com'
+                                                 ' according to query and optional'
+                                                 ' search terms in jobs description')
+    # arguments
+    parser.add_argument('query', metavar='query', type=str, help='Query string. Example: "software engineer"')
+    parser.add_argument('-t', '--terms', type=str, action='store', help='Search terms for job description separated by ",".'
+                                                                        'Example: "dog, cat lover, high quality"')
+    parser.add_argument('-n', '--num_jobs', action='store', type=int, help='Number of search results to fetch. Default: 15')
+    parser.add_argument('-sbr', '--sortby_relevance', action='store_true',
+                        help='Sort by relevance. Default is by date')
+    parser.add_argument('-a', '--all_terms', action='store_true',
+                        help='If specified, all search terms must be found in job description')
+    parser.set_defaults(sortby_relevance=False)
+    args = parser.parse_args()
 
-df = myjob.build_jobs_table()
+    if args.sortby_relevance:
+        myjob = Notifier(query=args.query, terms=args.terms, num_recent_jobs=args.num_jobs,
+                         sort_by='relevance', all_terms=args.all_terms)
+    else:
+        myjob = Notifier(query=args.query, terms=args.terms, num_recent_jobs=args.num_jobs,
+                         all_terms=args.all_terms)
 
-if df is None:
-    logger.info("No results were found")
-    print("No matching results were found!")
-else:
-    with open(ROOT_DIR + f'/jobs_{CURRENT_DATE}.html', 'w') as file:
-        file.write(df.to_html(justify='center', render_links=True, index=False))
-    logger.info(f"HTML file saved successfully to {ROOT_DIR}")
-    print(f'HTML file written successfully to {ROOT_DIR}')
+    df = myjob.build_jobs_table()
+
+    if df is None:
+        logger.info("No results were found")
+        print("No matching results were found!")
+    else:
+        with open(ROOT_DIR + f'/jobs_{CURRENT_DATE}.html', 'w') as file:
+            file.write(df.to_html(justify='center', render_links=True, index=False))
+        logger.info(f"HTML file saved successfully to {ROOT_DIR}")
+        print(f'HTML file written successfully to {ROOT_DIR}')
